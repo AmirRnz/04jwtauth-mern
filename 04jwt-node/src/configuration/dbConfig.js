b2d8003bf7bf92c7923c4da8471a026f45ea53de
AmirRnz <155143890+AmirRnz@users.noreply.github.com>
2025-03-25 20:02:52 +0330

    basics setup

---
+++ 04jwt-node/src/configuration/dbConfig.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+mongoose.connect("mongodb://localhost:27017/04jwt-node");
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongodb");
+});
+mongoose.connection.on("error", (error) => {
+  console.log(`mongodb connection error ${error}`);
+});
+
+export default mongoose;
