af960c4f561294fb7de415af60addce6c38aa8a2
AmirRnz <155143890+AmirRnz@users.noreply.github.com>
2025-04-06 14:43:59 +0330

    login page

--- 04jwt-react/src/components/Header.tsx
+++ 04jwt-react/src/components/Header.tsx
@@ -1,14 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
   return (
-    <nav className=" bg-gray-300 shadow-md">
+    <nav className="bg-gray-300 shadow-md">
       <div className="max-w-5xl flex justify-between gap-4 p-4 items-center">
         <Link to="/">Home</Link>
-        <div className="flex gap-4">
-          <Link to="/login">Login</Link>
-          <Link to="/register">Signup</Link>
-        </div>
+        {token ? (
+          <button onClick={handleLogOut}>SignOut</button>
+        ) : (
+          <div className="flex gap-4">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Signup</Link>
+          </div>
+        )}
       </div>
     </nav>
   );
--- 04jwt-react/src/pages/auth/login/Login.tsx
+++ 04jwt-react/src/pages/auth/login/Login.tsx
@@ -1,5 +1,86 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const formData = { email, password };
+      const response = await fetch("http://localhost:5000/auth/login", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const result = await response.json();
+      localStorage.setItem("token", result.token);
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-6 rounded-lg shadow-md w-80">
+        <h1 className="text-2xl font-bold text-center mb-4">Signup</h1>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          {/* Email */}
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              required
+            />
+          </div>
+
+          {/* Password */}
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              required
+            />
+          </div>
+
+          {/* Submit */}
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
